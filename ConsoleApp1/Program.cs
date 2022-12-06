cc0d6b72355a7185c1a0df825c3002c2c52eae59
z21ista21 <z21ista21@z00p1-05.SPSKNM>
2022-12-06 08:10:01 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book LOTR = new Book();
+            LOTR.title = "PPSH";
+            Console.WriteLine(LOTR.title);
+        }
+    }
+}
